67d0556fbf88281a2531eb89046fb469d96b1ba8
piyush1way <86835187+piyush1way@users.noreply.github.com>
2025-03-19 01:43:16 +0530

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -446,119 +446,119 @@
 #             x = F.normalize(x, dim=1)
 #         return self.fc(x)
 
-# '''ResNet in PyTorch.
-# For Pre-activation ResNet, see 'preact_resnet.py'.
-# Reference:
-# [1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-#     Deep Residual Learning for Image Recognition. arXiv:1512.03385
-# '''
-# import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
-# import torchvision
-# from models.build import ENCODER_REGISTRY
+'''ResNet in PyTorch.
+For Pre-activation ResNet, see 'preact_resnet.py'.
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from models.build import ENCODER_REGISTRY
 
 
-# class BasicBlock(nn.Module):
-#     expansion = 1
+class BasicBlock(nn.Module):
+    expansion = 1
 
-#     def __init__(self, in_planes, planes, stride=1, use_bn_layer=False, Conv2d=nn.Conv2d):
-#         super(BasicBlock, self).__init__()
-#         self.conv1 = Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-#         self.bn1 = nn.GroupNorm(2, planes) if not use_bn_layer else nn.BatchNorm2d(planes) 
-#         self.conv2 = Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-#         self.bn2 = nn.GroupNorm(2, planes) if not use_bn_layer else nn.BatchNorm2d(planes) 
+    def __init__(self, in_planes, planes, stride=1, use_bn_layer=False, Conv2d=nn.Conv2d):
+        super(BasicBlock, self).__init__()
+        self.conv1 = Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.GroupNorm(2, planes) if not use_bn_layer else nn.BatchNorm2d(planes) 
+        self.conv2 = Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.GroupNorm(2, planes) if not use_bn_layer else nn.BatchNorm2d(planes) 
 
-#         self.downsample = nn.Sequential()
-#         if stride != 1 or in_planes != self.expansion * planes:
-#             self.downsample = nn.Sequential(
-#                 Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-#                 nn.GroupNorm(2, self.expansion * planes) if not use_bn_layer else nn.BatchNorm2d(planes)
-#             )
+        self.downsample = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.downsample = nn.Sequential(
+                Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.GroupNorm(2, self.expansion * planes) if not use_bn_layer else nn.BatchNorm2d(planes)
+            )
 
-#     def forward(self, x: torch.Tensor, no_relu: bool = False) -> torch.Tensor:
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = self.bn2(self.conv2(out))
-#         out += self.downsample(x)
-#         if not no_relu:
-#             out = F.relu(out)
-#         return out
-
-
-# class Bottleneck(nn.Module):
-#     expansion = 4
-
-#     def __init__(self, in_planes, planes, stride=1, use_bn_layer=False, Conv2d=nn.Conv2d):
-#         super(Bottleneck, self).__init__()
-#         self.conv1 = Conv2d(in_planes, planes, kernel_size=1, bias=False)
-#         self.bn1 = nn.GroupNorm(2, planes) if not use_bn_layer else nn.BatchNorm2d(planes)
-#         self.conv2 = Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-#         self.bn2 = nn.GroupNorm(2, planes) if not use_bn_layer else nn.BatchNorm2d(planes)
-#         self.conv3 = Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
-#         self.bn3 = nn.GroupNorm(2, self.expansion * planes) if not use_bn_layer else nn.BatchNorm2d(planes)
-
-#         self.downsample = nn.Sequential()
-#         if stride != 1 or in_planes != self.expansion * planes:
-#             self.downsample = nn.Sequential(
-#                 Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-#                 nn.GroupNorm(2, self.expansion * planes) if not use_bn_layer else nn.BatchNorm2d(planes)
-#             )
-
-#     def forward(self, x: torch.Tensor, no_relu: bool = False) -> torch.Tensor:
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = F.relu(self.bn2(self.conv2(out)))
-#         out = self.bn3(self.conv3(out))
-#         out += self.downsample(x)
-#         if not no_relu:
-#             out = F.relu(out)
-#         return out
+    def forward(self, x: torch.Tensor, no_relu: bool = False) -> torch.Tensor:
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.downsample(x)
+        if not no_relu:
+            out = F.relu(out)
+        return out
 
 
-# class ResNet(nn.Module):
-#     def __init__(self, block, num_blocks, num_classes=10, l2_norm=False, use_pretrained=False, use_bn_layer=False,
-#                  last_feature_dim=512, **kwargs):
-#         super(ResNet, self).__init__()
-#         self.l2_norm = l2_norm
-#         self.in_planes = 64
-#         conv1_kernel_size = 3 if not use_pretrained else 7
+class Bottleneck(nn.Module):
+    expansion = 4
 
-#         Conv2d = self.get_conv()
-#         self.conv1 = Conv2d(3, 64, kernel_size=conv1_kernel_size, stride=1, padding=1, bias=False)
-#         self.bn1 = nn.GroupNorm(2, 64) if not use_bn_layer else nn.BatchNorm2d(64)
+    def __init__(self, in_planes, planes, stride=1, use_bn_layer=False, Conv2d=nn.Conv2d):
+        super(Bottleneck, self).__init__()
+        self.conv1 = Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.GroupNorm(2, planes) if not use_bn_layer else nn.BatchNorm2d(planes)
+        self.conv2 = Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.GroupNorm(2, planes) if not use_bn_layer else nn.BatchNorm2d(planes)
+        self.conv3 = Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.GroupNorm(2, self.expansion * planes) if not use_bn_layer else nn.BatchNorm2d(planes)
 
-#         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, use_bn_layer=use_bn_layer)
-#         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, use_bn_layer=use_bn_layer)
-#         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, use_bn_layer=use_bn_layer)
-#         self.layer4 = self._make_layer(block, last_feature_dim, num_blocks[3], stride=2, use_bn_layer=use_bn_layer)
+        self.downsample = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.downsample = nn.Sequential(
+                Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.GroupNorm(2, self.expansion * planes) if not use_bn_layer else nn.BatchNorm2d(planes)
+            )
 
-#         self.num_layers = 6
-#         self.fc = nn.Linear(last_feature_dim * block.expansion, num_classes)
+    def forward(self, x: torch.Tensor, no_relu: bool = False) -> torch.Tensor:
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.downsample(x)
+        if not no_relu:
+            out = F.relu(out)
+        return out
 
-#     def get_conv(self):
-#         return nn.Conv2d
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10, l2_norm=False, use_pretrained=False, use_bn_layer=False,
+                 last_feature_dim=512, **kwargs):
+        super(ResNet, self).__init__()
+        self.l2_norm = l2_norm
+        self.in_planes = 64
+        conv1_kernel_size = 3 if not use_pretrained else 7
+
+        Conv2d = self.get_conv()
+        self.conv1 = Conv2d(3, 64, kernel_size=conv1_kernel_size, stride=1, padding=1, bias=False)
+        self.bn1 = nn.GroupNorm(2, 64) if not use_bn_layer else nn.BatchNorm2d(64)
+
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, use_bn_layer=use_bn_layer)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, use_bn_layer=use_bn_layer)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, use_bn_layer=use_bn_layer)
+        self.layer4 = self._make_layer(block, last_feature_dim, num_blocks[3], stride=2, use_bn_layer=use_bn_layer)
+
+        self.num_layers = 6
+        self.fc = nn.Linear(last_feature_dim * block.expansion, num_classes)
+
+    def get_conv(self):
+        return nn.Conv2d
     
-#     def _make_layer(self, block, planes, num_blocks, stride, use_bn_layer=False):
-#         strides = [stride] + [1] * (num_blocks - 1)
-#         layers = []
-#         for stride in strides:
-#             layers.append(block(self.in_planes, planes, stride, use_bn_layer=use_bn_layer, Conv2d=self.get_conv()))
-#             self.in_planes = planes * block.expansion
-#         return nn.Sequential(*layers)
+    def _make_layer(self, block, planes, num_blocks, stride, use_bn_layer=False):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride, use_bn_layer=use_bn_layer, Conv2d=self.get_conv()))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
 
-#     def forward(self, x, return_feature=False):
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = self.layer1(out)
-#         out = self.layer2(out)
-#         out = self.layer3(out)
-#         out = self.layer4(out)
+    def forward(self, x, return_feature=False):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
 
-#         out = F.adaptive_avg_pool2d(out, 1)
-#         out = out.view(out.size(0), -1)
-#         logit = self.fc(out)
+        out = F.adaptive_avg_pool2d(out, 1)
+        out = out.view(out.size(0), -1)
+        logit = self.fc(out)
 
-#         if return_feature:
-#             return out, logit
-#         return logit
+        if return_feature:
+            return out, logit
+        return logit
 
 
 #  Personalized FedRCL Model with a Client-Specific Head

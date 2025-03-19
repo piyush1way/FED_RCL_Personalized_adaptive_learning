463bd75b522b793a154cbffb412eaa977f8d318e
piyush1way <86835187+piyush1way@users.noreply.github.com>
2025-03-19 10:00:06 +0530

    Update __init__.py

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -1,3 +1,5 @@
 from trainers.build import get_trainer_type
 
-from trainers.base_trainer import Trainer
+# from trainers.base_trainer import Trainer
+from trainers.base_trainer import BaseTrainer as Trainer
+

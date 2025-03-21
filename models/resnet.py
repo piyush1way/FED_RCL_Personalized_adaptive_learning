d70bc8b0e8026e317e265964a1b3a6bd3f6175f1
piyush1way <86835187+piyush1way@users.noreply.github.com>
2025-03-21 19:51:14 +0530

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -222,7 +222,7 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        
+    
         out = F.adaptive_avg_pool2d(out, 1)
         features = out.view(out.size(0), -1)
         

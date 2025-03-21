5098416c0326b32b3339f236afb8f1994a17e68e
piyush1way <86835187+piyush1way@users.noreply.github.com>
2025-03-21 23:56:10 +0530

    Update __init__.py

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -1,8 +1,8 @@
+# trainers/__init__.py
 from trainers.build import TRAINER_REGISTRY, get_trainer_type
 from trainers.base_trainer import BaseTrainer
 
 # Register any additional trainer classes here if needed
-# from .base_trainer import BaseTrainer
 from .personalized_trainer import PersonalizedTrainer
 
-__all__ = ['BaseTrainer', 'PersonalizedTrainer']
+__all__ = ['BaseTrainer', 'PersonalizedTrainer', 'TRAINER_REGISTRY', 'get_trainer_type']

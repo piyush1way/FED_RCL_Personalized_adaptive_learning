2b946ebaae52fdb47ef88c25cbd199d75f2ac41d
piyush1way <piyushwanve10218@gmail.com>
2025-03-26 04:44:51 +0530

    Auto-update from Cursor AI

---
+++ auto_push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/Downloads/NOVELITY/updtaed/FED_RCL_Personalized_adaptive_learning  # Move to repo
+git add .  # Add all changes
+git commit -m "Auto-update from Cursor AI"
+git push origin main  # Push changes
